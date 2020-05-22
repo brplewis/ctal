98b138326b158518c22d4a1f6deacc74fe5b10ad
Bob <bobrplewis@gmail.com>
2020-05-22 15:57:43 +0100

    Add create_update_message tests

--- connections/test_logger.py
+++ connections/test_logger.py
@@ -88,13 +88,42 @@ class TestTeradiciLoggerStatus(unittest.TestCase):
             "2020-05-21T14:40:58.060Z 00000000-0000-0000-0000-000000000000 > LVL:2 RC:   0           AGENT :send_message, 8 bytes",
             "2020-05-21T14:40:58.160Z 00000000-0000-0000-0000-000000000000 > LVL:2 RC:   0           AGENT :cSERVER_SESSION::send_acknowledgement succeeded to srvr0005",
             "2020-05-21T14:42:29.302Z 00000000-0000-0000-0000-000000000000 > LVL:2 RC:   0           AGENT :cSERVER_SESSION::agent_receiver_callback: message from A:srvr5;B:srvr0005 to A:srvr5, message = 16 00 00 00, len=20",
-            "2020-05-21T14:40:55.433Z f684cd80-7d9e-1038-a745-000000000000 > LVL:2 RC:   0           AGENT :1754 SSO: user DOMAIN\John.Williams has activated, single sign on completed."]
-        #self.assertEqual(tal_logger.check_status(test_list)[2], "INVALID")
-        # test for User_name when before message
+            ]
+        self.assertEqual(tal_logger.check_status(test_list)[2], "INVALID")
+        # test for User_name when after message
         self.assertEqual(tal_logger.check_status(test_list)[1], "DOMAIN\John.Williams")
+        test_list = [
+            "2020-05-21T14:40:55.433Z f684cd80-7d9e-1038-a745-000000000000 > LVL:2 RC:   0           AGENT :1754 SSO: user DOMAIN\John.Williams has activated, single sign on completed.",
+            "2020-05-21T14:40:58.058Z f684cd80-7d9e-1038-a745-000000000000 > LVL:2 RC:   0           AGENT :1244 -------------------------------------------------------------------",
+            "2020-05-21T14:40:58.059Z 00000000-0000-0000-0000-000000000000 > LVL:1 RC:   0           AGENT :-----------------------------------------------------------------------",
+            "2020-05-20T19:21:53.699Z 00000000-0000-0000-0000-000000000000 > LVL:1 RC:   0           AGENT :transition from STOPPING -- STOPPING --(SERVER_STOPPED [202])--> INVALID",
+            "2020-05-21T14:40:58.060Z 00000000-0000-0000-0000-000000000000 > LVL:1 RC:   0           AGENT :-----------------------------------------------------------------------",
+            "2020-05-21T14:40:58.060Z 00000000-0000-0000-0000-000000000000 > LVL:2 RC:   0           AGENT :cSERVER_SESSION::send_acknowledgement sending ack.",
+            "2020-05-21T14:40:58.060Z 00000000-0000-0000-0000-000000000000 > LVL:2 RC:   0           AGENT :send_message, 8 bytes",
+            "2020-05-21T14:40:58.160Z 00000000-0000-0000-0000-000000000000 > LVL:2 RC:   0           AGENT :cSERVER_SESSION::send_acknowledgement succeeded to srvr0005",
+            "2020-05-21T14:42:29.302Z 00000000-0000-0000-0000-000000000000 > LVL:2 RC:   0           AGENT :cSERVER_SESSION::agent_receiver_callback: message from A:srvr5;B:srvr0005 to A:srvr5, message = 16 00 00 00, len=20",
+            "2020-05-21T14:40:55.433Z f684cd80-7d9e-1038-a745-000000000000 > LVL:2 RC:   0           AGENT :1754 SSO: user DOMAIN\John.Williams has activated, single sign on completed."]
         # Test TypeError Str
-        self.assertEqual(tal_logger.check_status("Test list"), "Input is not a Teradici Log list")
+        self.assertEqual(tal_logger.check_status("Test list"), "Input is not a Teradici log list")
         # Test TypeError int
-        self.assertEqual(tal_logger.check_status(2), "Input is not a Teradici Log list")
+        self.assertEqual(tal_logger.check_status(2), "Input is not a Teradici log list")
         # Test TypeError float
-        self.assertEqual(tal_logger.check_status(2.01), "Input is not a Teradici Log list")
+        self.assertEqual(tal_logger.check_status(2.01), "Input is not a Teradici log list")
+
+class TestTeradiciLoggerMessage(unittest.TestCase):
+    def test_create_update_report(self):
+        tal_logger = TeradiciLogger('wlringest10')
+        # Test Connected message
+        self.assertEqual(tal_logger.create_update_message([['2020-05-21', '14:40:58'], 'DOMAIN\John.Williams', 'CONNECTED']), '2020-05-21 14:40:58 | wlringest10 is CONNECTED | Active User : DOMAIN\John.Williams')
+        # Test Disconnected message
+        tal_logger.user_name = 'DOMAIN\John.Williams'
+        self.assertEqual(tal_logger.create_update_message([['2020-05-21', '14:40:58'], '', 'INVALID']), '2020-05-21 14:40:58 | wlringest10 is DISCONNECTED | Last User : DOMAIN\John.Williams')
+        # Check for wrong list error
+        self.assertEqual(tal_logger.create_update_message(['Something', 20, 'Wrong']), "Error: Entered list does not appear to be a 'session_variable' list")
+        # Test TypeError Str
+        self.assertEqual(tal_logger.create_update_message("Test list"), "Input is not a Teradici log list")
+        # Test TypeError int
+        self.assertEqual(tal_logger.create_update_message(2), "Input is not a Teradici log list")
+        # Test TypeError float
+        self.assertEqual(tal_logger.create_update_message(2.01), "Input is not a Teradici log list")
+
